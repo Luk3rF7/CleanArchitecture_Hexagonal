31516e4777df87bd0d3a483daf2859801a6e3532
Luk3rF7 <lkzeron@gmail.com>
2024-01-24 22:48:38 -0300

    Intro ao arquitetura limpa e hexagonal

---
+++ 00_Intro_fundamentos/00_codeVsArquitetura.js
@@ -0,0 +1,15 @@
+/** 
+*  Existe 3 caso :
+* 
+* ? #1 usar ForEach 
+* ? #2 usar map 
+* ? #3 usar reducer 
+* 
+* * algum da questao e relevante a arquitetura ?
+* 
+*  !R:  não e uma questao relevante 
+*
+* - Arquitetura - alto custo $$$
+* - Designer -  custo de mudança e  mais baixo $$ 
+* - Codigo  - não deve causar empacto no designer e arquitetura
+*/
