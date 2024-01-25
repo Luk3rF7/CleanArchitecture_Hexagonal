350fce5ce35bd1d8df39b5254bfd274f78fa1947
Luk3rF7 <lkzeron@gmail.com>
2024-01-24 23:41:08 -0300

    Layers Camadas  da aplicacao

---
+++ 00_Intro_fundamentos/05_PadraoCamada_Layers.js
@@ -0,0 +1,26 @@
+/**
+ *  Layers - Dividir em Camadas :
+ * 
+ *  ? Camada Apresentação :
+ *   - View 
+ *     Onde fica nosso Front end,javascript e css 
+ * 
+ * ? Camada de Negocio : 
+ *  onde fica nossa Regra de Negocio ou da aplicação 
+ * 
+ * ? Camada de infra : 
+ *   DataBase,Email,Services 
+ * 
+ * Apresentação depende da camada de negocio 
+ * Camada de Negocio denpende da camada de infra 
+ * E a infra e nossa interface onde acessa MySql da vida,envio de email 
+ * 
+ * ? Camada Comun : 
+ * onde recebe todas classes que recebe dados 
+ * 
+ * todo - Protocolo TCP/IP :
+ *  ? Solid - D 
+ *    
+ *
+ *  
+ */
