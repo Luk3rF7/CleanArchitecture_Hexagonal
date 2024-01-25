1ba96a4f28274385fff4dec8ba48392ee55816c8
Luk3rF7 <lkzeron@gmail.com>
2024-01-24 22:49:02 -0300

    Estruturada

---
+++ 00_Intro_fundamentos/02_ParadigmaEstruturada.js
@@ -0,0 +1,10 @@
+/**
+ *  Paradigma Estruturada :
+ *   - Estrutura de controle :
+ *     - if/else
+ *     - for/while 
+ *  - Procedimento ou Procedual :
+ *  - Diciplina aplicada de transferencia direta de  controle
+ *  conten pilha de execução :
+ * 
+ */
