b6b251f264df703bcaa743cd440be27ce4a9ac17
Luk3rF7 <lkzeron@gmail.com>
2024-01-24 23:02:27 -0300

    Paradigma Orientado a Objetos

---
+++ 00_Intro_fundamentos/03_ParadigmaPOO.js
@@ -0,0 +1,30 @@
+/**
+ *  Paradigma POO : 
+ * ? # 3 Pilares : 
+ * 
+ *    1 - Encapsulamento
+ *    2 - Herança
+ *    3 - Polimofismo
+ * 
+ * ? # Encapsulamento :
+ *   Esconder os Detalhes 
+ *  
+ *   Objeto dentro da estrutura teremos :
+ * 
+ *  - Atributtos :
+ *  - Comportamento (método) :
+ * 
+ * ? # Herança :
+ * 
+ *   ___ e um(a) __ = relação de herança .
+ *   ___ tem um(a) __ = relação de composição .
+ *       priorizar o da composição !
+ *
+ * ? # Polimorfismo :
+ * 
+ *   determinado obj que representa um carro ,que precisa de outra
+ *   class para reutilizar e sobrescrevemos metodo ,e multipla forma
+ *   que posso utilizar:
+ *  
+ * todo - Diciplina transferencia indireta de  controle  do Polimorfismo 
+*/
