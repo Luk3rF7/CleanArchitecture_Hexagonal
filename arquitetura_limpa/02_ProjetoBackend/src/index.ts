4abc77eecd67349123108fd8939a51464d05eee3
Luk3rF7 <lkzeron@gmail.com>
2024-01-26 22:04:02 -0300

    nomeclatura  e iniciando backend  com hexagonal arquitetura limpa

--- arquitetura_limpa/02_ProjetoBackend/src/index.ts
+++ arquitetura_limpa/02_ProjetoBackend/src/index.ts
@@ -1,3 +1,20 @@
-export function somar(a: number, b: number) {
-    return a + b
-}
+import dotenv from "dotenv"
+import express from "express"
+dotenv.config()
+
+const app = express()
+const port = process.env.API_PORT || 4000
+
+// Arquivo json :
+app.use(express.json())
+// Arquivo Formulario :
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.send("<h1> Arquiterura Hexagonal !! </h1>")
+})
+// excutando backend
+app.listen(port, () => {
+    console.log("backend iniciado...")
+    console.log(`http://localhost:${port}`)
+})
