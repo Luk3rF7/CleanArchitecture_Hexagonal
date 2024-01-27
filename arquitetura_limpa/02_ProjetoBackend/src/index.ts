41f9c0a6c6588ebd51124eb2ead663e2edfbbe9f
Luk3rF7 <lkzeron@gmail.com>
2024-01-26 22:12:54 -0300

    backend com arquitetura hexagonal

--- arquitetura_limpa/02_ProjetoBackend/src/index.ts
+++ arquitetura_limpa/02_ProjetoBackend/src/index.ts
@@ -1,20 +1,47 @@
 import dotenv from "dotenv"
-import express from "express"
 dotenv.config()
 
-const app = express()
-const port = process.env.API_PORT || 4000
+import express from "express"
+import RegistrarUsuario from "./core/usuario/service/RegistrarUsuario"
+import RepositorioUsuarioPg from "./external/db/RepositorioUsuarioPg"
+import SenhaCripto from "./external/auth/SenhaCripto"
+import RegistrarUsuarioController from "./external/api/RegistrarUsuarioController"
+import LoginUsuario from "./core/usuario/service/LoginUsuario"
+import LoginUsuarioController from "./external/api/LoginUsuarioController"
+import ObterProdutoPorId from "./core/produto/service/ObterProdutoPorId"
+import ObterProdutoPorIdController from "./external/api/ObterProdutoPorIdController"
+import UsuarioMiddleware from "./external/api/UsuarioMiddleware"
 
-// Arquivo json :
+const app = express()
+const porta = process.env.API_PORT ?? 4000
+
 app.use(express.json())
-// Arquivo Formulario :
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-    res.send("<h1> Arquiterura Hexagonal !! </h1>")
+app.listen(porta, () => {
+    console.log(`🔥 Servidor executando na porta ${porta}!`)
 })
-// excutando backend
-app.listen(port, () => {
-    console.log("backend iniciado...")
-    console.log(`http://localhost:${port}`)
-})
+
+// ------------------------------------ Rotas Abertas
+
+const repositorioUsuario = new RepositorioUsuarioPg()
+const provedorCripto = new SenhaCripto()
+
+const registrarUsuario = new RegistrarUsuario(
+    repositorioUsuario,
+    provedorCripto
+)
+
+const loginUsuario = new LoginUsuario(
+    repositorioUsuario,
+    provedorCripto
+)
+
+new RegistrarUsuarioController(app, registrarUsuario)
+new LoginUsuarioController(app, loginUsuario)
+
+// ------------------------------------ Rotas Protegidas
+const usuarioMid = UsuarioMiddleware(repositorioUsuario)
+
+const obterProdutoPorId = new ObterProdutoPorId()
+new ObterProdutoPorIdController(app, obterProdutoPorId, usuarioMid)
